37ebbbba19afac50afeb4483d808a806981584d9
Muhammad Fuzail <159969929+MuhammadFuzail591@users.noreply.github.com>
2024-10-01 16:34:58 -0400

    Update script.js

--- ReactJS week 1/Day2 JSX and Source Maps/lib/script.js
+++ ReactJS week 1/Day2 JSX and Source Maps/lib/script.js
@@ -1,1 +1,10 @@
+// const h2 = <h2>Using JSX</h2>;
+// ReactDOM.render(h2, document.getElementById('root'));
 
+//How to use JavaScript inside JSX
+
+const userName = "M Fuzail";
+const h1 = /*#__PURE__*/React.createElement("h1", null, "Hello ", /*#__PURE__*/React.createElement("b", null, userName), " ");
+ReactDOM.render(h1, document.getElementById('root'));
+console.log(h1);
+//# sourceMappingURL=script.js.map
