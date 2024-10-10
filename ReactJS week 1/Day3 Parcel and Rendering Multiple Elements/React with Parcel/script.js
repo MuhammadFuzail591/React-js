192262c0d47a46286467016356c6cd847f834aad
Muhammad Fuzail <159969929+MuhammadFuzail591@users.noreply.github.com>
2024-10-10 09:12:47 -0400

    Add files via upload

---
+++ ReactJS week 1/Day3 Parcel and Rendering Multiple Elements/React with Parcel/script.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+console.log(React)
+
+const heading = <h1>Hello World using JSX</h1>
+
+console.log(heading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(heading)
+
+console.log("Muhammad Fuzail");
+
